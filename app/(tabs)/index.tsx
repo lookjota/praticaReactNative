f1990784934d8389790a7ec432bd6845db76e1ae
lookjota <lookjota@gmail.com>
2024-08-21 14:13:41 -0300

    Utilizando Status bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,18 @@
+
+import { StatusBar } from 'react-native';
 import { Home } from '../../src/screens/Home'
 
 export default function TabOneScreen() {
   return (
-    <Home/>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home/>
+    </>
+    
   );
 }
 
